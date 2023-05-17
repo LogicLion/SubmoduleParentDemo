467eb4f5e09f667eb9fa3bef6f344857d9d8bbc5
zhanhua <913447093@qq.com>
2023-05-17 11:11:36 +0800

    主模块测试提交

--- app/src/main/java/com/example/fathersondemo/MainActivity.kt
+++ app/src/main/java/com/example/fathersondemo/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!，version:")
-    println("测试提交")
+    println("测试提交测试提交测试提交测试提交")
 }
 
 @Preview(showBackground = true)
